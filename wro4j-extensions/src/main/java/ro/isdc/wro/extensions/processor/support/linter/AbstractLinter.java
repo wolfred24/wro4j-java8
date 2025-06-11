8a9aa0a1d5c9039dbb981774c2c359151075d4fc
Cesar Alfredo Martinez Gallegos <Cesar.Alfredo.Martinez@ibm.com>
2025-06-10 21:35:39 -0600

    refactor: improve logging and code formatting in AbstractLinter

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/linter/AbstractLinter.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/linter/AbstractLinter.java
@@ -22,7 +22,6 @@ import ro.isdc.wro.util.WroUtil;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-
 /**
  * Encapsulates common code for utilities like jsHint or jsLint.
  *
@@ -31,124 +30,143 @@ import com.google.gson.reflect.TypeToken;
  * @since 1.4.2
  */
 public abstract class AbstractLinter {
-  private static final Logger LOG = LoggerFactory.getLogger(AbstractLinter.class);
-  /**
-   * Required to make jshint not complain.
-   */
-  private static final String DEFINE_WINDOW = "var window = {};";
-  private WebjarUriLocator webjarLocator;
-  private final OptionsBuilder optionsBuilder = new OptionsBuilder();
-  /**
-   * Options to apply to js hint processing
-   */
-  private String options;
-  private RhinoScriptBuilder builder;
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractLinter.class);
+    /**
+     * Required to make jshint not complain.
+     */
+    private static final String DEFINE_WINDOW = "var window = {};";
+    private WebjarUriLocator webjarLocator;
+    private final OptionsBuilder optionsBuilder = new OptionsBuilder();
+    /**
+     * Options to apply to js hint processing
+     */
+    private String options;
+    private RhinoScriptBuilder builder;
 
-  /**
-   * Initialize script builder for evaluation.
-   */
-  private RhinoScriptBuilder initScriptBuilder() {
-    try {
-      if (builder == null) {
-        builder = RhinoScriptBuilder.newChain().evaluateChain(DEFINE_WINDOW, "window").evaluateChain(
-            getScriptAsStream(), "linter.js");
-      }
-      return builder;
-    } catch (final IOException e) {
-      throw new WroRuntimeException("Failed reading init script", e);
+    /**
+     * Initialize script builder for evaluation.
+     */
+    private RhinoScriptBuilder initScriptBuilder() {
+        try {
+            if (builder == null) {
+                builder = RhinoScriptBuilder.newChain().evaluateChain(DEFINE_WINDOW, "window").evaluateChain(
+                        getScriptAsStream(), "linter.js");
+            }
+            return builder;
+        } catch (final IOException e) {
+            throw new WroRuntimeException("Failed reading init script", e);
+        }
     }
-  }
 
-  /**
-   * @return {@link WebjarUriLocator} instance to retrieve webjars.
-   */
-  protected final WebjarUriLocator getWebjarLocator() {
-    if (webjarLocator == null) {
-      webjarLocator = new WebjarUriLocator();
+    /**
+     * @return {@link WebjarUriLocator} instance to retrieve webjars.
+     */
+    protected final WebjarUriLocator getWebjarLocator() {
+        if (webjarLocator == null) {
+            webjarLocator = new WebjarUriLocator();
+        }
+        return webjarLocator;
     }
-    return webjarLocator;
-  }
 
-  /**
-   * @return the stream of the linter script. Override this method to provide a different script version.
-   * @throws IOException
-   *           if the stream is invalid or unavailable.
-   */
-  protected abstract InputStream getScriptAsStream()
-      throws IOException;
+    /**
+     * @return the stream of the linter script. Override this method to provide a
+     *         different script version.
+     * @throws IOException
+     *                     if the stream is invalid or unavailable.
+     */
+    protected abstract InputStream getScriptAsStream()
+            throws IOException;
 
-  /**
-   * Validates a js using jsHint and throws {@link LinterException} if the js is invalid. If no exception is thrown, the
-   * js is valid.
-   *
-   * @param data
-   *          js content to process.
-   */
-  public void validate(final String data)
-      throws LinterException {
-    final StopWatch watch = new StopWatch();
-    watch.start("init");
-    final RhinoScriptBuilder builder = initScriptBuilder();
-    watch.stop();
-    watch.start("lint");
-    final String packIt = buildLinterScript(WroUtil.toJSMultiLineString(data), getOptions());
-    final boolean valid = Boolean.parseBoolean(builder.evaluate(packIt, "check").toString());
-    if (!valid) {
-      final String json = builder.addJSON().evaluate(
-          String.format("JSON.stringify(JSON.decycle(%s.errors))", getLinterName()), "stringify errors").toString();
-      LOG.debug("json {}", json);
-      final Type type = new TypeToken<List<LinterError>>() {}.getType();
-      final List<LinterError> errors = new Gson().fromJson(json, type);
-      LOG.debug("errors {}", errors);
-      throw new LinterException().setErrors(errors);
+    /**
+     * Validates a js using jsHint and throws {@link LinterException} if the js is
+     * invalid. If no exception is thrown, the
+     * js is valid.
+     *
+     * @param data
+     *             js content to process.
+     */
+    public void validate(final String data)
+            throws LinterException {
+        LOG.debug("[DEBUG] Validando JS que inicia con: "
+                + data.substring(0, Math.min(40, data.length())).replace("\n", " ") + "...");
+        final StopWatch watch = new StopWatch();
+        watch.start("init");
+        final RhinoScriptBuilder builder = initScriptBuilder();
+        watch.stop();
+        watch.start("lint");
+        final String packIt = buildLinterScript(WroUtil.toJSMultiLineString(data), getOptions());
+        if (!packIt.contains(",{});")) { // Solo imprime si hay opciones
+            LOG.debug("[DEBUG] Script executed in JSHint: " + packIt);
+        }
+        // Execute JShint.
+        final boolean valid = Boolean.parseBoolean(builder.evaluate(packIt, "check").toString());
+        if (!valid) {
+            final String json = builder.addJSON().evaluate(
+                    String.format("JSON.stringify(JSON.decycle(%s.errors))", getLinterName()), "stringify errors")
+                    .toString();
+            LOG.debug("json {}", json);
+            final Type type = new TypeToken<List<LinterError>>() {
+            }.getType();
+            final List<LinterError> errors = new Gson().fromJson(json, type);
+            LOG.debug("[DEBUG] Errores JSHint:");
+            if (errors != null) {
+                for (LinterError err : errors) {
+                    if (err != null) {
+                        LOG.debug(String.format("  - Line %d, Char %d: %s%n    Evidence: %s%n",
+                                err.getLine(), err.getCharacter(), err.getReason(), err.getEvidence()));
+                    }
+                }
+            }
+            LOG.debug("errors {}", errors);
+            throw new LinterException().setErrors(errors);
+        }
+        LOG.debug("result: {}", valid);
+        watch.stop();
+        LOG.debug(watch.prettyPrint());
     }
-    LOG.debug("result: {}", valid);
-    watch.stop();
-    LOG.debug(watch.prettyPrint());
-  }
 
-  /**
-   * @return the name of the function used to perform the lint operation.
-   */
-  protected abstract String getLinterName();
+    /**
+     * @return the name of the function used to perform the lint operation.
+     */
+    protected abstract String getLinterName();
 
-  /**
-   * TODO this method is duplicated in {@link CssLint}. Extract and reuse it.
-   *
-   * @param data
-   *          script to process.
-   * @param options
-   *          options to set as true
-   * @return Script used to pack and return the packed result.
-   */
-  private String buildLinterScript(final String data, final String options) {
-    return String.format("%s(%s,%s);", getLinterName(), data, optionsBuilder.buildFromCsv(options));
-  }
-
-  /**
-   * @param options
-   *          the options to set
-   */
-  public AbstractLinter setOptions(final String... options) {
-    this.options = StringUtils.join(options, ',');
-    LOG.debug("setOptions: {}", this.options);
-    return this;
-  }
-
-  /**
-   * @return an options as CSV.
-   */
-  private String getOptions() {
-    if (options == null) {
-      options = createDefaultOptions();
+    /**
+     * TODO this method is duplicated in {@link CssLint}. Extract and reuse it.
+     *
+     * @param data
+     *                script to process.
+     * @param options
+     *                options to set as true
+     * @return Script used to pack and return the packed result.
+     */
+    private String buildLinterScript(final String data, final String options) {
+        return String.format("%s(%s,%s);", getLinterName(), data, optionsBuilder.buildFromCsv(options));
     }
-    return options;
-  }
 
-  /**
-   * @return default options to use for linting.
-   */
-  protected String createDefaultOptions() {
-    return "";
-  }
+    /**
+     * @param options
+     *                the options to set
+     */
+    public AbstractLinter setOptions(final String... options) {
+        this.options = StringUtils.join(options, ',');
+        LOG.debug("setOptions: {}", this.options);
+        return this;
+    }
+
+    /**
+     * @return an options as CSV.
+     */
+    private String getOptions() {
+        if (options == null) {
+            options = createDefaultOptions();
+        }
+        return options;
+    }
+
+    /**
+     * @return default options to use for linting.
+     */
+    protected String createDefaultOptions() {
+        return "";
+    }
 }

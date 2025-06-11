3364c060f8b01457182301980eec77d409891b75
Cesar Alfredo Martinez Gallegos <Cesar.Alfredo.Martinez@ibm.com>
2025-06-10 21:36:17 -0600

    test: add test for globals option parsing in JsHintProcessor

--- wro4j-extensions/src/test/java/ro/isdc/wro/extensions/processor/TestJsHintProcessor.java
+++ wro4j-extensions/src/test/java/ro/isdc/wro/extensions/processor/TestJsHintProcessor.java
@@ -5,6 +5,8 @@ package ro.isdc.wro.extensions.processor;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.junit.Assert;
@@ -19,7 +21,6 @@ import ro.isdc.wro.model.resource.processor.ResourcePostProcessor;
 import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 import ro.isdc.wro.util.WroTestUtils;
 
-
 /**
  * Test {@link JsHintProcessor}.
  *
@@ -27,83 +28,100 @@ import ro.isdc.wro.util.WroTestUtils;
  * @created Created on Feb 27, 2011
  */
 public class TestJsHintProcessor
-    extends AbstractTestLinterProcessor {
-  private JsHintProcessor victim;
+        extends AbstractTestLinterProcessor {
+    private JsHintProcessor victim;
 
-  @Before
-  public void setUp() {
-    victim = new JsHintProcessor();
-  }
+    @Before
+    public void setUp() {
+        victim = new JsHintProcessor();
+    }
 
-  @Override
-  protected ResourcePreProcessor newLinterProcessor() {
-    return new JsHintProcessor();
-  }
+    @Override
+    protected ResourcePreProcessor newLinterProcessor() {
+        return new JsHintProcessor();
+    }
 
-  @Test
-  public void testWithOptionsSet()
-      throws Exception {
-    final ThreadLocal<Throwable> cause = new ThreadLocal<Throwable>();
+    @Test
+    public void testWithOptionsSet()
+            throws Exception {
+        final ThreadLocal<Throwable> cause = new ThreadLocal<Throwable>();
 
-    final ResourcePostProcessor processor = new JsHintProcessor() {
-      @Override
-      protected void onLinterException(final LinterException e, final Resource resource) {
-        cause.set(e);
-      };
-    }.setOptionsAsString("maxerr=1");
+        final ResourcePostProcessor processor = new JsHintProcessor() {
+            @Override
+            protected void onLinterException(final LinterException e, final Resource resource) {
+                cause.set(e);
+            };
+        }.setOptionsAsString("maxerr=1");
 
-    processor.process(new StringReader("alert(;"), new StringWriter());
-    Assert.assertNotNull(cause.get());
-  }
+        processor.process(new StringReader("alert(;"), new StringWriter());
+        Assert.assertNotNull(cause.get());
+    }
 
-  /**
-   * This test was created initially to prove that {@link JsHintProcessor} is thread-safe, but it doesn't work well when
-   * trying to reuse the scope. TODO: This needs to be investigated.
-   */
-  @Test
-  public void canBeExecutedMultipleTimes()
-      throws Exception {
-    final JsHintProcessor processor = new JsHintProcessor();
-    final Callable<Void> task = new Callable<Void>() {
-      @Override
-      public Void call() {
-        try {
-          processor.process(new StringReader("alert(1);"), new StringWriter());
-        } catch (final Exception e) {
-          throw new RuntimeException(e);
-        }
-        return null;
-      }
-    };
-    WroTestUtils.runConcurrently(task);
-  }
+    @Test
+    public void testGlobalsOptionIsParsedCorrectly() throws Exception {
+        final ThreadLocal<Throwable> cause = new ThreadLocal<Throwable>();
+        // Usa una variable global que normalmente marcaría error
+        String js = "dojo = 1;\ndefine = function(){};\nale rt(1);\nalert(;";
+        final ResourcePostProcessor processor = new JsHintProcessor() {
+            @Override
+            protected void onLinterException(final LinterException e, final Resource resource) {
+                cause.set(e);
+            }
+        }.setOptionsAsString("esnext=true,undef=true,predef=['dojo','define']");
 
-  @Test
-  public void shouldSupportCorrectResourceTypes() {
-    WroTestUtils.assertProcessorSupportResourceTypes(new JsHintProcessor(), ResourceType.JS);
-  }
+        processor.process(new StringReader(js), new StringWriter());
+        Assert.assertNotNull(cause.get());
+    }
 
-  @Test
-  public void canSetNullOptions()
-      throws Exception {
-    victim.setOptionsAsString("");
-    victim.process(null, new StringReader("alert(1);"), new StringWriter());
-  }
+    /**
+     * This test was created initially to prove that {@link JsHintProcessor} is
+     * thread-safe, but it doesn't work well when
+     * trying to reuse the scope. TODO: This needs to be investigated.
+     */
+    @Test
+    public void canBeExecutedMultipleTimes()
+            throws Exception {
+        final JsHintProcessor processor = new JsHintProcessor();
+        final Callable<Void> task = new Callable<Void>() {
+            @Override
+            public Void call() {
+                try {
+                    processor.process(new StringReader("alert(1);"), new StringWriter());
+                } catch (final Exception e) {
+                    throw new RuntimeException(e);
+                }
+                return null;
+            }
+        };
+        WroTestUtils.runConcurrently(task);
+    }
 
-  @Test(expected = LinterException.class)
-  public void shouldOverrideDefaultOptions()
-      throws Exception {
-    victim = new JsHintProcessor() {
-      @Override
-      protected String createDefaultOptions() {
-        return "bitwise";
-      }
+    @Test
+    public void shouldSupportCorrectResourceTypes() {
+        WroTestUtils.assertProcessorSupportResourceTypes(new JsHintProcessor(), ResourceType.JS);
+    }
 
-      @Override
-      protected void onLinterException(final LinterException e, final Resource resource) {
-        throw e;
-      }
-    };
-    victim.process(null, new StringReader("true & false"), new StringWriter());
-  }
+    @Test
+    public void canSetNullOptions()
+            throws Exception {
+        victim.setOptionsAsString("");
+        victim.process(null, new StringReader("alert(1);"), new StringWriter());
+    }
+
+    @Test(expected = LinterException.class)
+    public void shouldOverrideDefaultOptions()
+            throws Exception {
+        victim = new JsHintProcessor() {
+            @Override
+            protected String createDefaultOptions() {
+                return "bitwise";
+            }
+
+            @Override
+            protected void onLinterException(final LinterException e, final Resource resource) {
+                throw e;
+            }
+        };
+        victim.process(null, new StringReader("true & false"), new StringWriter());
+    }
 }
